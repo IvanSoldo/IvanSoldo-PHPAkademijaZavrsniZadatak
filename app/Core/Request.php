8d3d22bd2ec65f86040021deb8987fff3501115d
IvanSoldo <ivandragosoldo@gmail.com>
2020-10-02 23:32:35 +0200

    htmlspecialchars validation implemented

--- app/Core/Request.php
+++ app/Core/Request.php
@@ -6,7 +6,7 @@ class Request
 {
     public static function getPostParam($key, $default='')
     {
-        return $_POST[$key] ?? htmlspecialchars($default, ENT_QUOTES, 'UTF-8');
+        return $_POST[$key] ?? $default;
     }
 
     public static function getPostParams()
--- app/Util/sessionHelper.php
+++ app/Util/sessionHelper.php
@@ -15,8 +15,8 @@ function flash ($name = '', $message = '', $class = 'alert alert-success') {
             if (!empty($_SESSION[$name . '_class'])){
                 unset($_SESSION[$name . '_class']);
             }
-            $_SESSION[$name] = $message;
-            $_SESSION[$name . '_class'] = $class;
+            $_SESSION[$name] = e($message);
+            $_SESSION[$name . '_class'] = e($class);
         } elseif (empty($message) && !empty($_SESSION[$name])) {
             $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
             echo '<div class ="'.$class.'" id="msg-flash">'. $_SESSION[$name].'</div> ';
