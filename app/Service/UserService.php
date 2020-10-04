104234830c576e3ffc163664770b58c9a5c8526a
IvanSoldo <ivandragosoldo@gmail.com>
2020-10-04 21:43:23 +0200

    Homepage Images bug fixed/Favicon implemented

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -144,14 +144,6 @@ class UserService
     public function logout()
     {
         $_SESSION = array();
-
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time()-1800,
-            $params['path'], $params['domain'],
-            $params['secure'], $params['httponly']);
-        }
-        session_destroy();
         header('location: ' . URLROOT . '/User/login');
     }
 
